d49fa7e6e76f8bf3c2c2e054c50c928fd64cea6c
marc7666 <64266158+marc7666@users.noreply.github.com>
2022-05-30 13:37:05 +0200

    Exercise 6 Observer type pull

---
+++ src/Class_exercises/Exercise_6/Store.java
@@ -0,0 +1,17 @@
+package Class_exercises.Exercise_6;
+
+/**
+ * @project: Design-pattern-examples.
+ * @author: mcr99 on 30/05/2022.
+ */
+public class Store {
+    private String name;
+
+    public Store(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
